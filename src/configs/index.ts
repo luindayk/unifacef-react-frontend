84aca6f7f3a5ee01c6a4c47b64257ade079acc8b
Willyan Luindayk <willyanluindayk@gmail.com>
2020-07-04 11:26:36 -0300

    Consult CEP correios

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,10 @@
 export const configs = {
   apis: {
     economia: process.env.REACT_APP_ECONOMIA_URL,
-    starWars: process.env.REACT_APP_STAR_WARS_BASEURL
+    starWars: process.env.REACT_APP_STAR_WARS_BASEURL,
+    correios: process.env.REACT_APP_CEP_URL,
+    github: process.env.REACT_APP_GITHUB_URL
   },
   sentry: process.env.REACT_APP_SENTRY_DSN,
-  onesignal: process.env.REACT_APP_ONE_SIGNAL!
+  onesignal: process.env.REACT_APP_ONE_SIGNAL!,
 }
